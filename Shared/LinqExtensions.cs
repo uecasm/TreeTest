88ff0b4965e21f35a224c9a614ff49c89da4f70b
Gavin Lambert <uecasm@users.noreply.github.com>
2022-06-27 14:17:02 +1200

    Initial import

---
+++ Shared/LinqExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace TreeTest.Shared;
+
+internal static class LinqExtensions
+{
+    public static IEnumerable<T> SelectRecursive<T>(this IEnumerable<T>? list, Func<T, IEnumerable<T>?> childSelector)
+    {
+        var tree = new Stack<T>((list ?? Enumerable.Empty<T>()).Reverse());
+
+        while (tree.Count > 0)
+        {
+            var current = tree.Pop();
+            yield return current;
+
+            foreach (var child in (childSelector(current) ?? Enumerable.Empty<T>()).Reverse())
+            {
+                tree.Push(child);
+            }
+        }
+    }
+}
